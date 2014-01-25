498fa79465ca79521a17bd3f771514a1a22d28a4
Wes Pickett <wes.pickett@gmail.com>
2014-01-25 03:39:48 -0500

    database models for shirt

--- pyrfectClothes/models.py
+++ pyrfectClothes/models.py
@@ -1,10 +1,32 @@
 from django.db import models
 
 # Create your models here.
-class MaleShirt(models.Model):
+class Shirt(models.Model):
     arm_length = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
     neck_circumference = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
     shoulders_circumference = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
     arm_circumference_upper = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
     chest_circumference = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
     waist_circumference = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+
+    class Meta:
+        abstract = True
+    
+class MaleShirt(Shirt):
+    pass
+
+class FemaleShirt(Shirt):
+    bust = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+
+class MalePants(models.Model):
+    waist_circumference = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+    butt_circumference = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+    leg_circumference_upper = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+    leg_length_outer = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+    leg_length_inner = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+
+class MaleShoes(models.Model):
+    foot_size_length = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+    foot_size_width = models.DecimalField(default=0.0,max_digits=5,decimal_places=2)
+    
+ 
